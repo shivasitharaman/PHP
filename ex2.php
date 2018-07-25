5bca2e7c309e0f8d96ec20ca3469758467aead7a
shivasitharaman <40603401+shivasitharaman@users.noreply.github.com>
2018-07-25 21:10:01 +0530

    Add files via upload

---
+++ ex2.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $x = "Hello world!";
+$y = 'Hello world!';
+echo $x;
+echo "<br>"; 
+echo $y;echo "<br>"; 
+$x = 5985;
+var_dump($x);
+        ?>
+    </body>
+</html>
---
+++ ex6.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
+
+Welcome.php
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+       Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+    </body>
+</html>
